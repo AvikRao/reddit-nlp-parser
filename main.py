0dafac8be8d7156befda3711f98fd1bf19d28d4b
acidepicice <2021arao@tjhsst.edu>
2021-02-27 12:50:00 -0500

    Pull data from subreddits

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 import praw
 import json
+import pandas as pd
+
+SUBREDDITS = ("investing", "stocks")
 
 with open("auth.json", "r") as f :
     auth = json.loads(f.read())
 
 reddit = praw.Reddit(client_id=auth["client_id"], client_secret=auth["client_secret"], user_agent=auth["user_agent"])
 
-posts = reddit.subreddit('investing').top(limit=1)
-for post in posts :
-    print(post.title)
+data = set()
+
+for subreddit in SUBREDDITS :
+
+    praw_subreddit = reddit.subreddit(subreddit)
+
+    top_posts_week = praw_subreddit.top("week")
+    for post in top_posts_week :
+        data.add(post)
+    
+    hot_posts = praw_subreddit.hot()
+    for post in hot_posts :
+        data.add(post)
+
+print(len(data))
+
+'''
+elements in `data`: https://praw.readthedocs.io/en/latest/code_overview/models/submission.html
+'''
